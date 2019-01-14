f5c51a825bd5efc1beaff4901cee0dcc4e88d51e
Dylan Thomson <djthomson88@gmail.com>
2019-01-14 16:49:55 -0500

    Start index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const Word = require("./Word");
+const Inquirer = require("inquirer");
+
+// const word = new Word("testword");
+// word.guessLetter("W");
+// console.log(word.getWord());
+
+const testWords = ["apple", "banana", "orange"]
